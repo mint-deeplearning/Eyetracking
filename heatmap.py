0b231a418c6222a008f7676dc5277ec5b670f06d
mint <53610737+mint-deeplearning@users.noreply.github.com>
2025-11-28 11:51:20 +0800

    Add files via upload

---
+++ heatmap.py
@@ -0,0 +1,191 @@
+import os
+import argparse
+import csv
+import cv2
+import numpy
+# import matplotlib
+from matplotlib import pyplot, image
+import pandas as pd
+import PIL
+def draw_display(dispsize, imagefile=None):
+
+ 
+    screen = numpy.zeros((dispsize[1], dispsize[0], 3), dtype='float32')
+
+    if imagefile != None:
+       
+        if not os.path.isfile(imagefile):
+            raise Exception("imagefile not found at '%s'" % imagefile)
+
+        img = image.imread(imagefile)
+
+        
+        w, h = len(img[0]), len(img)
+
+        x = int(dispsize[0] / 2 - w / 2)
+        y = int(dispsize[1] / 2 - h / 2)
+        
+        screen[y:y + h, x:x + w, :] += img
+    
+    dpi = 100.0
+    
+    figsize = (dispsize[0] / dpi, dispsize[1] / dpi)
+    
+    fig = pyplot.figure(figsize=figsize, dpi=dpi, frameon=False)
+    ax = pyplot.Axes(fig, [0, 0, 1, 1])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+
+    ax.axis([0, dispsize[0], 0, dispsize[1]])
+    ax.imshow(screen)  # , origin='upper')
+
+    return fig, ax
+
+def draw_display_1(dispsize, backgroundImg):
+    
+
+    # construct screen (black background)
+    screen = numpy.zeros((dispsize[1], dispsize[0], 3), dtype='float32')
+    # if an image location has been passed, draw the image
+    # if imagefile != None:
+    #     # check if the path to the image exists
+    #     if not os.path.isfile(imagefile):
+    #         raise Exception("ERROR in draw_display: imagefile not found at '%s'" % imagefile)
+    #     # load image
+
+
+    # img = cv2.cvtColor(backgroundImg, cv2.COLOR_BGR2RGB)
+    img = backgroundImg.copy()
+
+    # print('shape: ', img.shape)
+    # width and height of the image
+    w, h = len(img[0]), len(img)
+    # print('w :{}, h: {}'.format(w,h))
+    # x and y position of the image on the display
+    x = int(dispsize[0] / 2 - w / 2)
+    y = int(dispsize[1] / 2 - h / 2)
+    # draw the image on the screen
+    screen[y:y + h, x:x + w, :] += img
+
+    # cv2.imshow('screen.jpg', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
+    # cv2.waitKey(0)
+
+    # dots per inch
+    dpi = 300.0
+    # determine the figure size in inches
+    figsize = (dispsize[0] / dpi, dispsize[1] / dpi)
+    # create a figure
+    fig = pyplot.figure(figsize=figsize, dpi=dpi, frameon=False)
+    ax = pyplot.Axes(fig, [0, 0, 1, 1])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    # plot display
+    ax.axis([0, dispsize[0], 0, dispsize[1]])
+    # cv2.imshow('test.jpg', screen)
+    # cv2.waitKey(0)
+    ax.imshow(screen.astype('uint8'))
+    # ax.show()
+
+    # ax.imshow(screen/255)  # , origin='upper')
+
+    return fig, ax
+def gaussian(x, sx, y=None, sy=None):
+    
+    # square Gaussian if only x values are passed
+    if y == None:
+        y = x
+    if sy == None:
+        sy = sx
+    # centers
+    xo = x / 2
+    yo = y / 2
+    # matrix of zeros
+    M = numpy.zeros([y, x], dtype=float)
+    # gaussian matrix
+    for i in range(x):
+        for j in range(y):
+            M[j, i] = numpy.exp(
+                -1.0 * (((float(i) - xo) ** 2 / (2 * sx * sx)) + ((float(j) - yo) ** 2 / (2 * sy * sy))))
+
+    return M
+
+
+def draw_heatmap(gazepoints, dispsize, backgroundimg, alpha=0.5, savefilename=None, gaussianwh=200, gaussiansd=None):
+    
+    fig, ax = draw_display_1(dispsize, backgroundimg)
+
+
+    gwh = gaussianwh
+    # print('gwh: ', gwh)
+    # print('gwh: ', gwh.)
+    gsdwh = gwh / 6 if (gaussiansd is None) else gaussiansd
+    gaus = gaussian(gwh, gsdwh)
+    # matrix of zeroes
+    strt = int(gwh / 2)
+    heatmapsize = int(dispsize[1] + 2 * strt), int(dispsize[0] + 2 * strt)
+    # print('heatmapsize: ', heatmapsize)
+    heatmap = numpy.zeros(heatmapsize, dtype=float)#float
+    # create heatmap
+    for i in range(0, len(gazepoints)):
+        # get x and y coordinates
+
+        x = int(strt + gazepoints[i][0] - int(gwh / 2))
+        y = int(strt + gazepoints[i][1] - int(gwh / 2))
+        # print('x: {}, y: {}'.format(x, y))
+        # correct Gaussian size if either coordinate falls outside of
+        # display boundaries
+        if (not 0 < x < dispsize[0]) or (not 0 < y < dispsize[1]):
+            hadj = [0, gwh]
+            vadj = [0, gwh]
+            if 0 > x:
+                hadj[0] = abs(x)
+                x = 0
+            elif dispsize[0] < x:
+                hadj[1] = gwh - int(x - dispsize[0])
+            if 0 > y:
+                vadj[0] = abs(y)
+                y = 0
+            elif dispsize[1] < y:
+                vadj[1] = gwh - int(y - dispsize[1])
+            # add adjusted Gaussian to the current heatmap
+            try:
+                # print('vale: ', gaus[vadj[0]:vadj[1], hadj[0]:hadj[1]] * gazepoints[i][2])
+                heatmap[y:y + vadj[1], x:x + hadj[1]] += gaus[vadj[0]:vadj[1], hadj[0]:hadj[1]] * gazepoints[i][2]
+            except:
+                # fixation was probably outside of display
+                pass
+        else:
+            # add Gaussian to the current heatmap
+            heatmap[y:y + gwh, x:x + gwh] += gaus * gazepoints[i][2]
+    # resize heatmap
+    heatmap = heatmap[strt:dispsize[1] + strt, strt:dispsize[0] + strt]
+    # print('heatmap type: ', heatmap.dtype)
+    # cv2.imshow('heatmap2.jpg', heatmap)
+
+    # remove zeros
+    lowbound = numpy.mean(heatmap[heatmap > 0])
+    heatmap[heatmap < lowbound] = numpy.NaN
+    # draw heatmap on top of image
+
+    ax.imshow(heatmap, cmap='jet', alpha=alpha)
+
+    # ax.imshow(heatmap, cmap='jet', alpha=0.5)
+
+    # cv2.imshow('heatmap.jpg', heatmap)
+    # cv2.waitKey(0)
+    # PIL.Image.fromarray(heatmap).save('Example Output/res2.jpg')
+    # save = PIL.Image.fromarray(heatmap)
+    # if save.mode == "F":
+    #     save = save.convert('RGB')
+    # save.save('Example Output/res2.jpg')
+
+
+    # FINISH PLOT
+    # invert the y axis, as (0,0) is top left on a display
+    ax.invert_yaxis()
+    # save the figure if a file name was provided
+    if savefilename != None:
+        fig.savefig(savefilename)
+
+    pyplot.close('all')
+    # return fig
